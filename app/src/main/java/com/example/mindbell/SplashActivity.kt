aa2ba52ce1bc2fb37b7ee21c6f9a687057f0f148
Faditya <daffaaditya2607@gmail.com>
2026-03-06 01:56:00 +0700

    Feat: Add SplashActivity and configure splash screen assets and theme

---
+++ app/src/main/java/com/example/mindbell/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mindbell
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Durasi splash screen (misalnya 2 detik) sebelum pindah ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 2000 ms = 2 detik
+    }
+}
